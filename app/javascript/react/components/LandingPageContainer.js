aaa0eca1d40ef2e9e51768271453a2719efa9dbc
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2020-10-08 17:26:54 -0400

    added image looop

--- app/javascript/react/components/LandingPageContainer.js
+++ app/javascript/react/components/LandingPageContainer.js
@@ -1,4 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import banner from '../../../assets/images/banner.png'
+import backpacking from '../../../assets/images/backpacking.png'
+import camping from '../../../assets/images/camping.png'
+import hiking from '../../../assets/images/hiking.png'
 
+const LandingPageContainer = (props) =>{
 
+  const textArray = [<img alt="backpacking" src={backpacking} />, <img alt="camping" src={camping} />,<img alt="hiking" src={hiking} />]
+
+  const [seconds, setSeconds] = useState(0);
+  const [phrase, setPhrase] = useState(textArray[seconds % textArray.length])
+  const [loop, setLoop] = useState(true)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds(seconds => seconds + 1);
+    }, 3000);
+  }, []);
+
+  useEffect(() => {
+    if (loop === true) {
+      setPhrase(textArray[seconds % textArray.length])
+    }
+  })
+
+  return (
+    <div>
+    <div className="left column">
+    hello world words etc
+    </div>
+    <div className="right column">
+    {phrase}
+    </div>
+    <div className="bottom row">
+    </div>
+    </div>
+  )
+}
 export default LandingPageContainer
